ab1ff8430906edf73c7d043b7d8aede98e6fd2e7
Tony Tsai <tonyny58@gmail.com>
2021-03-15 16:36:30 +0800

    新增密碼儲存方式

--- homework4-2/app.py
+++ homework4-2/app.py
@@ -1,8 +1,15 @@
 from flask import request, render_template, url_for, redirect , Flask,session
+from datetime import timedelta
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.secret_key = '26adfff637e8343fd8d2a11a32ff454c'
+app.permanent_session_lifetime = timedelta(days=3)
+bcrypt = Bcrypt(app)
+p_hash = bcrypt.generate_password_hash('ttoooott').decode('utf-8')
+print(p_hash)
+print(bcrypt.check_password_hash(p_hash,"ttoooott"))
 
 
 users = {'test': {'password': 'test'}}
@@ -46,6 +53,7 @@ def login():
     
     user_id = request.form['user_id']
     if (user_id in users) and (request.form['password'] == users[user_id]['password']):
+        session.permanent = True
         session["user_name"] = user_id
         session["state"] = "已登入"
         return redirect(url_for('member'))
